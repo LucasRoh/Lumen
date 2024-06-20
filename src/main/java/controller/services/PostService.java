31dbf1adacc69d266bf9f02c050dc12fe9818818
Moritz Franze <moritz.franze@ict-campus.net>
2024-06-20 17:12:53 +0200

    created Post controller, service, model, repository

---
+++ src/main/java/controller/services/PostService.java
@@ -0,0 +1,45 @@
+package controller.services;
+
+import controller.repositories.BlogRepository;
+import controller.repositories.PostRepository;
+import models.Blog;
+import models.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+public class PostService {
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+//Nicht sicher ob int onder string
+    public void update(String id, Post post) {
+        if (!postRepository.existsById(id)) {
+            throw new EntityNotFoundException();
+        }
+
+        if (post.getId_post() == null) {
+            post.setId_post(id);
+        } else if (!post.getId_post().equals(id)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "id in URL does not match " +
+                    "ID in body!");
+        }
+
+        postRepository.save(post);
+    }
+
+    public Iterable<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    public Post findById(String id) {
+        Optional<Post> subject = postRepository.findById(id);
+        return subject.orElseThrow(EntityNotFoundException::new);
+    }
+}
--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,11 +1,61 @@
 package models;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import org.springframework.data.annotation.ReadOnlyProperty;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
+@Table(name = "post")
 public class Post {
+    @Id
+    @ReadOnlyProperty
+    @Column(name = "ID_Post")
+    private String id_post;
+
+    @NotBlank
+    @NotNull
+    private String answer;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_Blog")
+    private Blog blog;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_User")
+    private User user;
+
+    public String getId_post() {
+        return id_post;
+    }
+
+    public void setId_post(String id_post) {
+        this.id_post = id_post;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Blog getBlog() {
+        return blog;
+    }
+
+    public void setBlog(Blog blog) {
+        this.blog = blog;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
