1dba80414d6895de54da650980e7ef0950629ba3
Moritz Franze <moritz.franze@ict-campus.net>
2024-06-20 13:27:28 +0200

    Post model

---
+++ src/main/java/models/Post.java
@@ -0,0 +1,11 @@
+package models;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Post {
+}
