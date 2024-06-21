b8414215d0ad86e2ff6f37e06ed8a11b51c48385
Janis Pirktl <janis.pirktl@ict-campus.net>
2024-06-21 14:42:05 +0200

    add delete and create to post and bugfix comments

--- src/main/java/net/ictcampus/lumen_backend/web/rest/CommentController.java
+++ src/main/java/net/ictcampus/lumen_backend/web/rest/CommentController.java
@@ -1,14 +1,13 @@
 package net.ictcampus.lumen_backend.web.rest;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import net.ictcampus.lumen_backend.entities.Blog;
 import net.ictcampus.lumen_backend.service.CommentService;
 import net.ictcampus.lumen_backend.entities.Comment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.EntityNotFoundException;
@@ -18,45 +17,53 @@ import javax.validation.Valid;
 @Tag(name = "Comments")
 @RestController
 @RequestMapping("/comments")
+@RequiredArgsConstructor
 public class CommentController {
+
     private final CommentService commentService;
 
-    @Autowired
-    public CommentController(CommentService commentService) {
-        this.commentService = commentService;
-    }
-
-
-    public Iterable<Comment> findAll() {
+    @GetMapping(path = "")
+    public Iterable<Comment> getAll() {
         try {
             return commentService.findAll();
         } catch (EntityNotFoundException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Users not found");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No comment found!");
         }
     }
 
-    public void insertComment(@Valid @RequestBody Comment comment) {
+    @GetMapping(path = "{id}")
+    public Comment get(@PathVariable Integer id) {
         try {
-            commentService.insertAbility(comment);
-        } catch (RuntimeException e) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "Conflict");
+            return commentService.findById(id);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Comment not found!");
         }
     }
 
-    public void updateAbility(@Valid @RequestBody Comment comment) {
+    @PutMapping(path = "{id}", consumes = "application/json")
+    public void update(@PathVariable Integer id, @Valid @RequestBody Comment comment) {
         try {
-            commentService.updateAbility(comment);
+            commentService.update(id, comment);
         } catch (RuntimeException e) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT);
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Failed to update comment!");
         }
-
     }
 
-    public void deleteById(@RequestParam("id") Integer id) {
+    @PostMapping(path = "", consumes = "application/json")
+    public void create(@Valid @RequestBody Comment comment) {
+        try {
+            commentService.create(comment);
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Failed to create comment!");
+        }
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void delete(@PathVariable Integer id) {
         try {
             commentService.deleteById(id);
         } catch (RuntimeException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Failed to delete comment!");
         }
     }
 }
--- src/main/java/net/ictcampus/lumen_backend/web/rest/PostController.java
+++ src/main/java/net/ictcampus/lumen_backend/web/rest/PostController.java
@@ -1,5 +1,8 @@
 package net.ictcampus.lumen_backend.web.rest;
 
+import lombok.RequiredArgsConstructor;
+import net.ictcampus.lumen_backend.entities.Blog;
+import net.ictcampus.lumen_backend.entities.Comment;
 import net.ictcampus.lumen_backend.service.PostService;
 import net.ictcampus.lumen_backend.entities.Post;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +15,10 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/posts")
+@RequiredArgsConstructor
 public class PostController {
-    private final PostService postService;
 
-@Autowired
-    public PostController(PostService postService) {
-        this.postService = postService;
-    }
+    private final PostService postService;
 
     @GetMapping(path = "")
     public Iterable<Post> getAll() {
@@ -44,6 +44,33 @@ public class PostController {
             postService.update(id, post);
         } catch (RuntimeException e) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Failed to update post!");
+        }
+    }
+
+    @GetMapping(path = "{id}/posts")
+    public Iterable<Comment> getComments(@PathVariable Integer id) {
+        try {
+            return postService.findById(id).getComments();
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Post not found!");
+        }
+    }
+
+    @PostMapping(path = "", consumes = "application/json")
+    public void create(@Valid @RequestBody Post post) {
+        try {
+            postService.create(post);
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Failed to create post!");
+        }
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void delete(@PathVariable Integer id) {
+        try {
+            postService.deleteById(id);
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Failed to delete post!");
         }
     }
 }
