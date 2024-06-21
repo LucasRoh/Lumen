a3d41145e4fb039552cae864e2442ac008088475
phamthi <thi.pham@ict-campus.net>
2024-06-21 10:35:57 +0200

    Modified create and insert SQL

--- Database/create-db.sql
+++ Database/create-db.sql
@@ -2,63 +2,69 @@
 
 -- Drop constraints from POST table
 ALTER TABLE IF EXISTS POST DROP CONSTRAINT IF EXISTS fk_post_blog;
-ALTER TABLE IF EXISTS POST DROP CONSTRAINT IF EXISTS fk_post_benutzer;
+ALTER TABLE IF EXISTS POST DROP CONSTRAINT IF EXISTS fk_post_account;
 
 -- Drop constraints from COMMENT table
 ALTER TABLE IF EXISTS COMMENT DROP CONSTRAINT IF EXISTS fk_comment_post;
-ALTER TABLE IF EXISTS COMMENT DROP CONSTRAINT IF EXISTS fk_comment_benutzer;
+ALTER TABLE IF EXISTS COMMENT DROP CONSTRAINT IF EXISTS fk_comment_account;
 
 -- Drop constraints from BLOG table
-ALTER TABLE IF EXISTS BLOG DROP CONSTRAINT IF EXISTS fk_blog_benutzer;
+ALTER TABLE IF EXISTS BLOG DROP CONSTRAINT IF EXISTS fk_blog_account;
 
 -- Drop Tables
 
 DROP TABLE IF EXISTS COMMENT;
 DROP TABLE IF EXISTS POST;
 DROP TABLE IF EXISTS BLOG;
-DROP TABLE IF EXISTS BENUTZER;
+DROP TABLE IF EXISTS ACCOUNT;
 
 -- Recreate Tables
 
-CREATE TABLE BENUTZER (
-                          ID_Benutzer SERIAL PRIMARY KEY,
-                          Name VARCHAR(50),
-                          Password TEXT
+CREATE TABLE ACCOUNT (
+                         ID_Account SERIAL PRIMARY KEY,
+                         Name VARCHAR(50),
+                         Password TEXT
 );
 
 CREATE TABLE BLOG (
                       ID_Blog SERIAL PRIMARY KEY,
                       Title VARCHAR(100),
-                      Question TEXT,
-                      ID_Benutzer INT,
-                      CONSTRAINT fk_blog_benutzer
-                          FOREIGN KEY (ID_Benutzer) REFERENCES BENUTZER(ID_Benutzer)
+                      Question TEXT
+
+    ,ID_Account INT,
+                      CONSTRAINT fk_blog_account
+                          FOREIGN KEY (ID_Account) REFERENCES ACCOUNT(ID_Account)
+
 );
 
 CREATE TABLE POST (
                       ID_Post SERIAL PRIMARY KEY,
                       Answer TEXT,
                       ID_Blog INT,
-                      ID_Benutzer INT,
                       CONSTRAINT fk_post_blog
-                          FOREIGN KEY (ID_Blog) REFERENCES BLOG(ID_Blog),
-                      CONSTRAINT fk_post_benutzer
-                          FOREIGN KEY (ID_Benutzer) REFERENCES BENUTZER(ID_Benutzer)
+                          FOREIGN KEY (ID_Blog) REFERENCES BLOG(ID_Blog)
+
+    ,ID_Account INT,
+                      CONSTRAINT fk_post_account
+                          FOREIGN KEY (ID_Account) REFERENCES ACCOUNT(ID_Account)
+
 );
 
 CREATE TABLE COMMENT (
                          ID_Comment SERIAL PRIMARY KEY,
                          Comment TEXT,
                          ID_Post INT,
-                         ID_Benutzer INT,
                          CONSTRAINT fk_comment_post
-                             FOREIGN KEY (ID_Post) REFERENCES POST(ID_Post),
-                         CONSTRAINT fk_comment_benutzer
-                             FOREIGN KEY (ID_Benutzer) REFERENCES BENUTZER(ID_Benutzer)
+                             FOREIGN KEY (ID_Post) REFERENCES POST(ID_Post)
+
+    ,ID_Account INT,
+                         CONSTRAINT fk_comment_account
+                             FOREIGN KEY (ID_Account) REFERENCES ACCOUNT(ID_Account)
+
 );
 
 
--- SELECT * FROM Comment;
--- SELECT * FROM POST;
--- SELECT * FROM BLOG;
--- SELECT * FROM Benutzer;
+SELECT * FROM Comment;
+SELECT * FROM POST;
+SELECT * FROM BLOG;
+SELECT * FROM Account;
